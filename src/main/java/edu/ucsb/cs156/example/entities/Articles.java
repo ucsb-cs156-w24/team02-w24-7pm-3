5570e6090aecaaed1da18076c197505128662db5
Jason Gu <1057337859@qq.com>
2024-02-06 22:12:24 -0800

    fixed migration file

--- src/main/java/edu/ucsb/cs156/example/entities/Articles.java
+++ src/main/java/edu/ucsb/cs156/example/entities/Articles.java
@@ -22,6 +22,7 @@ public class Articles {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
+  
   private String title;
   private String url;
   private String explanation;
--- src/main/java/edu/ucsb/cs156/example/repositories/ArticlesRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/ArticlesRepository.java
@@ -1,12 +1,11 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.User;
+import edu.ucsb.cs156.example.entities.Articles;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-  Optional<User> findByEmail(String email);
+public interface ArticlesRepository extends CrudRepository<Articles, Long> {
 }
