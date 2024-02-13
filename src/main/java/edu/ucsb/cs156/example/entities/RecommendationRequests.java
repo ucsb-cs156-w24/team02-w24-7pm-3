fe01c492b92008838bf5e3e608ba0f6f1de5af0b
Shamita Gurusu <shamita@ucsb.edu>
2024-02-13 10:12:30 -0800

    updated code coverage

--- src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequests.java
+++ src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequests.java
@@ -16,7 +16,7 @@ import lombok.Builder;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity(name = "recommendationrequest")
+@Entity(name = "recommendationrequests")
 public class RecommendationRequests  {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
